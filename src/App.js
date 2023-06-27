1ac89aa2c4534a6a8899ac56a9e33842869be120
Brian Zhou <brianhzhou@gmail.com>
2023-06-27 16:17:35 -0700

    add Hero

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
-      <div style={{height: "4rem"}}></div>
       <main>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
-          mollis lectus. Suspendisse finibus auctor mattis. Interdum et
-          malesuada fames ac ante ipsum primis in faucibus. Aenean sollicitudin
-          ultricies nisl non aliquam. Vestibulum metus erat, iaculis ac
-          vulputate eu, tristique non neque. Sed ultricies nunc sed libero
-          laoreet, sed interdum elit aliquam. Donec dictum turpis vitae augue
-          euismod sollicitudin. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos. Sed hendrerit blandit
-          nisi, id tincidunt quam facilisis eget. Sed rhoncus quis nulla nec
-          accumsan. Praesent arcu elit, pretium non lectus vel, iaculis gravida
-          mi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
-          mollis lectus. Suspendisse finibus auctor mattis. Interdum et
-          malesuada fames ac ante ipsum primis in faucibus. Aenean sollicitudin
-          ultricies nisl non aliquam. Vestibulum metus erat, iaculis ac
-          vulputate eu, tristique non neque. Sed ultricies nunc sed libero
-          laoreet, sed interdum elit aliquam. Donec dictum turpis vitae augue
-          euismod sollicitudin. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos. Sed hendrerit blandit
-          nisi, id tincidunt quam facilisis eget. Sed rhoncus quis nulla nec
-          accumsan. Praesent arcu elit, pretium non lectus vel, iaculis gravida
-          mi. Curabitur sollicitudin diam lacinia ultrices hendrerit. Curabitur
-          tincidunt, mi ac sagittis accumsan, magna ante sollicitudin purus, ac
-          ultrices nulla urna vel sem. Donec sollicitudin, risus ut pulvinar
-          aliquam, augue sapien sollicitudin lacus, vitae dictum neque tortor
-          eget erat. Vestibulum sit amet lectus malesuada, tempor tortor vel,
-          condimentum leo. Aliquam erat volutpat. Ut vulputate urna ac sapien
-          rutrum tempor. Vestibulum quam tellus, ultrices id nibh nec,
-          consectetur tincidunt sem. Duis justo urna, sollicitudin non elit ac,
-          vulputate egestas quam. Nam sollicitudin, velit sed mattis fringilla,
-          turpis mi tempus arcu, a tempus arcu elit finibus est. Fusce sit amet
-          odio sagittis, bibendum dolor ut, ultrices ante. Suspendisse mattis
-          quam et risus rutrum pulvinar eu tincidunt turpis. Vestibulum vitae
-          scelerisque neque, id venenatis tortor. Vestibulum semper est turpis,
-          nec mattis risus molestie non. Phasellus sapien nulla, rhoncus et
-          mauris eget, dictum lobortis nulla. Etiam nibh nunc, vestibulum et
-          hendrerit semper, pretium tempor sem. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Mauris imperdiet mattis venenatis.
-          Aenean sed enim vitae metus fermentum rhoncus. Donec sagittis dictum
-          egestas. Fusce quis iaculis mi. Praesent vestibulum pellentesque
-          tellus, ac laoreet mauris placerat a. Sed malesuada fringilla nibh
-          eget gravida. Suspendisse facilisis, mi condimentum faucibus maximus,
-          justo est aliquet nunc, sed iaculis lacus ante eu nibh.
-        </p>
+        <Hero />
       </main>
     </>
   );
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import classes from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={classes.background}>
+      <div class={classes.content}>
+        <h1>I am Brian</h1>
+        <p>(Insert Something Interesting Here)</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
