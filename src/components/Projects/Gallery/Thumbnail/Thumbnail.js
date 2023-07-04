85b3650d6fe5433dfc3e81c053a4e7332e1b01f3
Brian Zhou <brianhzhou@gmail.com>
2023-07-04 09:53:53 -0700

    update featured projects

--- src/components/Projects/Gallery/Thumbnail/Thumbnail.js
+++ src/components/Projects/Gallery/Thumbnail/Thumbnail.js
@@ -4,6 +4,7 @@ const Thumbnail = (props) => {
   return (
     <div className={classes["thumbnail-container"]}>
       <img src="https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_640.jpg" alt="..." draggable={false}/>
+      <p className={classes.badge}>In-Development!</p>
     </div>
   );
 };
--- src/components/Skills/SkillsList/SkillsList.js
+++ src/components/Skills/SkillsList/SkillsList.js
@@ -6,7 +6,7 @@ const SkillsList = (props) => {
     <SplitHalf>
       <SkillItem title="Javascript" skillLevel="Intermediate" percent="78%" />
       <SkillItem title="C++" skillLevel="Advanced" />
-      <SkillItem title="3" />
+      <SkillItem title="3" skillLevel="99%" />
       <SkillItem title="4" />
       <SkillItem title="5" />
     </SplitHalf>
