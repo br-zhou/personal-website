a37449439eeb7b620e6bd7a96a9ee1c786c05005
Brian Zhou <brianhzhou@gmail.com>
2023-07-04 10:24:50 -0700

    add project thumbnail hover effect

--- src/components/Projects/Gallery/Thumbnail/Thumbnail.js
+++ src/components/Projects/Gallery/Thumbnail/Thumbnail.js
@@ -4,6 +4,9 @@ const Thumbnail = (props) => {
   return (
     <div className={classes["thumbnail-container"]}>
       <img src="https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_640.jpg" alt="..." draggable={false}/>
+      <div className={classes.overlay}>
+        <h1>Click For More Information</h1>
+      </div>
       <p className={classes.badge}>In-Development!</p>
     </div>
   );
