1e541d33997e4cfea1ac890545372cf67c4ed172
Brian Zhou <brianhzhou@gmail.com>
2023-07-03 21:46:06 -0700

    update hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,6 +4,12 @@ const Hero = () => {
   return (
     <div className={classes.background}>
       <div className={classes.content}>
+        <img
+          src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+          alt="profile"
+          className={classes.picture}
+          draggable="false"
+        />
         <h1>I am Brian</h1>
         <p>(Insert Something Interesting Here)</p>
       </div>
