ede5b31e11a83d9e69a935076e81f86a721370c4
Brian Zhou <brianhzhou@gmail.com>
2023-07-05 15:11:37 -0700

    add sun to hero background

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,15 +3,19 @@ import classes from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={classes.background}>
-      <div className={classes.content}>
+      <div className={classes['bg-content']}>
+        <div className={classes.sun}></div>
+      </div>
+      
+      <div className={classes['main-content']}>
         <img
           src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
           alt="profile"
           className={classes.picture}
           draggable="false"
         />
-        <h1>I am Brian</h1>
-        <p>(Insert Something Interesting Here)</p>
+        <h1 className={classes.title}>Brian Zhou</h1>
+        <p>Student, Developer, and Food Lover</p>
       </div>
     </div>
   );
