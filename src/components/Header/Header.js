244018eb104ddbf86365ac7c4d89cdae22607fb4
Brian Zhou <brianhzhou@gmail.com>
2023-07-06 16:27:29 -0700

    add hamburger navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import logo from "../../assets/logox64.png";
+import BurgerSvg from "./BurgerSvg";
 import classes from "./Header.module.css";
+import XSvg from "./XSvg";
 
 const Header = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuToggle((lastState) => !lastState);
+  };
+
   return (
     <header className={classes.header}>
       <div className={classes["home-button"]}>
@@ -15,8 +24,21 @@ const Header = () => {
         <h2 className={classes["navbar-title"]}>Brian Zhou</h2>
       </div>
 
-      <nav className={classes["nav-links"]}>
+      <div>
+        <div class={classes["burger-btn"]} onClick={toggleMenu}>
+          <BurgerSvg size="24" />
+        </div>
+      </div>
+
+      <nav
+        className={`${classes["nav-links"]} ${
+          menuToggle && classes["show-side-bar"]
+        }`}
+      >
         <ul>
+          <li id={classes["burger-x-btn"]} onClick={toggleMenu}>
+            <XSvg size="48" />
+          </li>
           <li>
             <a href="/">Projects</a>
           </li>
