d0509030e65fbe111ca6a924dc45525480b3f6a9
Brian Zhou <brianhzhou@gmail.com>
2023-07-18 16:36:04 -0700

    update error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RootLayout from "./pages/RootLayout";
-import ErrorPage from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import ProjectsPage from "./pages/ProjectsPage";
 
 const router = createBrowserRouter([
--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
+import classes from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
-    <>
-      <h1>ERROR</h1>;
+    <div className={classes.main}>
+      <h1>An Error Occurred</h1>
+      <h4>404 Not Found</h4>;
       <p>
         Click <Link to="/">Here</Link> to return to the home page
       </p>
-    </>
+    </div>
   );
 };
 
