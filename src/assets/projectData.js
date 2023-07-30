ed8a66e4973e913a6d46f375b19e05736b1fff7a
Brian Zhou <brianhzhou@gmail.com>
2023-07-29 21:37:48 -0700

    update text for projects

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -2,14 +2,14 @@ export const FEATURED_PROJECTS = [
   {
     id: "ubc-avionics",
     title: "UBC Rocket Avionics",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "C++, FreeRTOS, MCUXpresso",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1133861176447615156/thumb.jpg",
   },
   {
     id: "dino-brawl",
     title: "Dino Brawl",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "JavaScript, Socket.IO, React.js, Node.js",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131341171982086154/image.png",
     badge: true,
@@ -17,28 +17,28 @@ export const FEATURED_PROJECTS = [
   {
     id: "csdc",
     title: "Canadian Satellite Design Challenge",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "C++, Arduino, PlatformIO",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1133857968077668544/THUMB.jpg",
   },
   {
     id: "candy-exterminator",
     title: "Candy Exterminator",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "C#, Unity",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131338549967474790/image.png",
   },
   {
     id: "app-blocker",
     title: "Python App Blocker",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Python, Command Line",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131340096621264906/image.png",
   },
   {
     id: "fh-clubs",
     title: "School Website",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "JQuery, Bootstrap, Javascript",
     thumb:
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131339690801381448/export.png",
   },
@@ -46,41 +46,46 @@ export const FEATURED_PROJECTS = [
 
 export const OTHER_PROJECTS = [
   {
-    id: "java-quiz-app",
-    title: "Java Quiz App",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
-  },
-  {
-    id: "black-jack",
-    title: "Black Jack",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
-  },
-  {
-    id: "flash-game",
-    title: "Space Fighter",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
-  },
-  {
-    id: "unity-game",
-    title: "3D Platformer",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    id: "site-tracker",
+    title: "Site Usage Tracker",
+    desc: "Javascript, Manifest V3",
   },
   {
     id: "personal-site",
     title: "Personal Website",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "React.js, Firebase, CSS",
   },
   {
-    id: "site-tracker",
-    title: "Site Usage Tracker",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    id: "flash-game",
+    title: "Space Fighter",
+    desc: "ActionScript 2, Adobe Flash",
+  },
+  {
+    id: "unity-game",
+    title: "3D Platformer",
+    desc: "C#, Unity",
+  },
+  {
+    id: "black-jack",
+    title: "Black Jack",
+    desc: "C++",
+  },
+  {
+    id: "java-quiz-app",
+    title: "Java Quiz App",
+    desc: "Java, Swing Library",
+  },
+  {
+    id: "physics-engine",
+    title: "Custom Physics Engine",
+    desc: "Javascript",
   },
 ];
 
 export const PROJECT_DETAILS = {
   "ubc-avionics": {
     title: "UBC Rocket Avionics",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Worked in a team to develop and test custom aerospace-grade electronics. Debugged mission-critical code and designed solutions for radio communications to space. Participated in bi-weekly meetings with the team to give updates and plan goals.",
     img: [
       "https://cdn.discordapp.com/attachments/803481006446870598/1134181009429119156/IMG_20221106_151538-min.jpg",
       "https://cdn.discordapp.com/attachments/803481006446870598/1134181009101955142/IMG_20221106_151535-min.jpg",
@@ -89,7 +94,7 @@ export const PROJECT_DETAILS = {
   },
   "dino-brawl": {
     title: "Dino Brawl",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Currently developing a online multiplayer game using Javascript, React.js, Socket.IO, and a custom game engine built from scratch.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131341171982086154/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134322124044111912/physics.jpg",
@@ -98,7 +103,7 @@ export const PROJECT_DETAILS = {
   },
   csdc: {
     title: "Canadian Satellite Design Challenge",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Coordinated with teammates to design and implement the onboard state machine, allowing the satellite to perform multiple missions during the same launch. We were chosen to represent Canada in the International CanSat Competition, held by the European Space Agency.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134230447547756574/compressed.jpg",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1133853147031425154/20220529_195455.jpg",
@@ -107,7 +112,7 @@ export const PROJECT_DETAILS = {
   },
   "candy-exterminator": {
     title: "Candy Exterminator",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Fully created for the 2022 $PLAY game jam, which spanned two weeks. Won first place. The game’s objective is to clear out levels by destroying evil gummy bears.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134217219237224509/main_menu.jpg",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134217443934482574/image.png",
@@ -116,8 +121,8 @@ export const PROJECT_DETAILS = {
     ],
   },
   "app-blocker": {
-    title: "Python App Blocker",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    title: "Python App & Site Blocker",
+    desc: "Utilized Python and the Windows Command Line to block applications and override DNS lookup. Designed an effective graphical interface for easy use of the application.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134215823653871626/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134216404627894313/image.png",
@@ -126,7 +131,7 @@ export const PROJECT_DETAILS = {
   },
   "fh-clubs": {
     title: "School Website",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Participating in clubs was a big part of my highschool experience. By trying out clubs, I was able to make new friends and discover my hobbies. I helped create this website because I wanted more students to express their passions through clubs.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134219004031029458/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134219173208276992/image.png",
@@ -136,14 +141,14 @@ export const PROJECT_DETAILS = {
   },
   "personal-site": {
     title: "Personal Website",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "This website was built from scratch using React and deployed through Google Firebase. Content is served through a CDN to improve loading time.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1131336875857170463/image.png",
     ],
   },
   "flash-game": {
     title: "Space Fighter",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Space Fighter was one of the first games I fully developed, which I did when I was 12. Although I have created a half-dozen games in Flash, Space Fighters is the most memorable due to the effort I put into creating the graphics. Sadly, most of my other games have been lost to time.",
     img: [
       "https://cdn.discordapp.com/attachments/803481006446870598/1134204114100826163/home_page.jpg",
       "https://cdn.discordapp.com/attachments/803481006446870598/1134207938177798306/image_2.jpg",
@@ -152,21 +157,41 @@ export const PROJECT_DETAILS = {
   },
   "java-quiz-app": {
     title: "Java Quiz App",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "This app helps students quiz themselves in preparation for tests. Users can create, edit, and take quizzes.",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134221332792487936/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134221263519363263/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134221493962813490/image.png",
     ],
   },
-
   "black-jack": {
     title: "Black Jack",
-    desc: "blah blach blach blach blach blah blach blach blach blach",
+    desc: "Black Jack is a terminal game developed in C++. ",
     img: [
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134223646253125684/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134223933277732945/image.png",
       "https://cdn.discordapp.com/attachments/1123397647785533561/1134224116283605042/image.png",
+    ],
+  },
+  "physics-engine": {
+    title: "Custom Physics & Game Engine",
+    desc: "Designed and implemented a custom game engine library that handles collision detection, collision response, and rendering logic. The library's API is inspired by the UnityEnigine API, making it intuitive for game developers.",
+  },
+  "site-tracker": {
+    title: "Site Usage Tracker",
+    desc: "I created this project using Google's new extension API, Manifest V3. This extension logs and displays browser usage. The data is automatically synced with other devices from the same Google account. This extension allows users to discover their bad habits.",
+    img: [
+      "https://cdn.discordapp.com/attachments/1123397647785533561/1135032204527931403/image.png",
+    ],
+  },
+
+  "unity-game": {
+    title: "3D Platformer",
+    desc: "This is my first Unity game, which I built when I was 13. The game is a simple 3d platformer, where the objective is to collect coins and reach the end of the level. The game was built using the aid of online tutorials.",
+    img: [
+      "https://cdn.discordapp.com/attachments/1123397647785533561/1135061010835832832/image.png",
+      "https://cdn.discordapp.com/attachments/1123397647785533561/1135060719340093491/image.png",
+      "https://cdn.discordapp.com/attachments/1123397647785533561/1135061944135598130/image.png",
     ],
   },
 };
