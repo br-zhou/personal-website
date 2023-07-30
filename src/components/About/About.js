5391fbb3fe41802574e77fdefdd187517754d086
Brian Zhou <brianhzhou@gmail.com>
2023-07-29 22:54:27 -0700

    remove hover for mobile

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,10 @@ import Button from "../UI/Button/Button";
 import classes from "./About.module.css";
 
 const About = () => {
+  const downloadCVHandler = () => {
+    console.log("download!")
+  }
+
   return (
     <section className={`bg-white section`}>
       <div className={`section-content ${classes.about}`}>
@@ -13,7 +17,7 @@ const About = () => {
           software engineering skills. As a curious individual, I am always
           looking for new challenges and opportunities!
         </p>
-        <Button text="Download CV" onClick={() => console.log("Download CV")} />
+        {/* <Button text="Download CV" onClick={downloadCVHandler} /> */}
       </div>
     </section>
   );
