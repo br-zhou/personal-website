4517c79874bcd8538ee3144083389a221e856ea4
Brian Zhou <brianhzhou@gmail.com>
2026-02-17 20:27:43 -0800

    update about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,9 @@ const About = () => {
       <div className={`section-content ${classes.about}`}>
         <h2>About Me</h2>
         <p>
-          Hello 👋! I'm a 4th year computer science student at the University of British Columbia.
-          My work experience involves software engineering and firmware development, usually with C++ and Python.
-          Beyone that, I enjoy making web projects and building games, and learning about cyber security and
-          low-level software.
+          Hello 👋! I'm a 4th year Computer Science student at UBC with a background in software engineering
+          and firmware development, primarily using C++ and Python. Beyond my professional work, I enjoy building
+          web games, and I'm passionate about cyber security and low-level software.
         </p>
         {/* <a
           href="https://cdn.br-zhou.com/Brian_Zhou_Resume.pdf"
